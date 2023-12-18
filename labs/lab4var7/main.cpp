2e250ec5e468df36e094bb4d74fc67ee664aa305
primky <fakeprimky@gmail.com>
2023-12-18 15:23:50 +0300

    init: added lab4var7

---
+++ labs/lab4var7/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size;
+    cout << "Enter array size: ";
+    cin >> size;
+
+    double* arr = new double[size];
+
+    cout << "Enter elements of array: " << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << "Element " << i << ": ";
+        cin >> arr[i];
+    }
+
+    int maxIndex = 0;
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    int minIndex = 0;
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
+        }
+    }
+
+    double temp = arr[0];
+    arr[0] = arr[maxIndex];
+    arr[maxIndex] = temp;
+
+    cout << "Result:\n";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+
+    return 0;
+}
