bad7dde1a1cd4ec136d7cb57def9637f7a99538a
primky <fakeprimky@gmail.com>
2023-12-19 11:27:45 +0300

    init:added lab7var7

---
+++ labs/lab7var7/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int countOnes(const string& str) {
+    int groups = 0;
+    int counterForOnes = 0;
+
+    for (char ch: str) {
+        if (ch != '0' && ch != '1') {cout << "[Error] Sting must contain only 1 or 0" << endl; ;return 0;}
+        if (ch == '1') {
+            counterForOnes++;
+            if (counterForOnes == 5) {
+                groups++;
+            }
+        } else {
+            if (counterForOnes > 5) {
+                groups--;
+            }
+            counterForOnes = 0;
+        }
+    }
+    cout << "Number of groups of ones is: " << groups << endl;
+    return groups;
+}
+
+int main() {
+    cout << "************************* Lab 6 var 7 **************************" << endl;
+    string inputString;
+    cout << "Enter sting that's consist of 1 or 0: " << endl;
+    cin >> inputString;
+    countOnes(inputString);
+    cout << "****************************************************************" << endl;
+    return 0;
+}
